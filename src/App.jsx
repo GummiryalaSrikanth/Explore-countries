2f54dd0bf99379b6c64aa1e0b1ac8881bd537a6d
Gummiryala Srikanth <gummiryalasrikanth369@gmail.com>
2024-09-14 16:08:01 +0530

    Explore-countries

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Searchbar from "./Searchbar";
+
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-center">Explore for Countries</h1>
+        <Searchbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
